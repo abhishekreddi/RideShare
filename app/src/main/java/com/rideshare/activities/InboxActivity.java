3807654f995c4ffdea7bf167d00e59a1b1c8525e
moparthim <moparthimahesh20416@gmail.com>
2021-02-15 18:17:18 -0500

    inbox activity updated...

--- app/src/main/java/com/rideshare/activities/InboxActivity.java
+++ app/src/main/java/com/rideshare/activities/InboxActivity.java
@@ -1,5 +1,7 @@
 package com.rideshare.activities;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -9,14 +11,14 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import com.rideshare.R;
 import com.rideshare.Utils;
 import com.rideshare.adapters.InboxAdapter;
+import com.rideshare.adapters.StudentAvailableRidesAdapter;
 import com.rideshare.api.ApiService;
 import com.rideshare.api.RetroClient;
 import com.rideshare.models.InboxPojo;
+import com.rideshare.models.RidesListPojo;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,6 +29,10 @@ import retrofit2.Response;
 
 public class InboxActivity extends AppCompatActivity {
     List<InboxPojo> inboxPojos;
+    InboxAdapter inboxAdapter;
+    ListView list_view;
+    ProgressDialog progressDialog;
+    EditText et_search;
     SharedPreferences sharedPreferences;
     String session;
 
@@ -48,4 +54,42 @@ public class InboxActivity extends AppCompatActivity {
         inboxPojos=new ArrayList<>();
         getReguestedRides();
     }
+    public void getReguestedRides(){
+        progressDialog = new ProgressDialog(InboxActivity.this);
+        progressDialog.setMessage("Loading....");
+        progressDialog.show();
+
+        ApiService service = RetroClient.getRetrofitInstance().create(ApiService.class);
+        Call<List<InboxPojo>> call = service.getmyrequests(session);
+        call.enqueue(new Callback<List<InboxPojo>>() {
+            @Override
+            public void onResponse(Call<List<InboxPojo>> call, Response<List<InboxPojo>> response) {
+                progressDialog.dismiss();
+                if(response.body()==null){
+                    Toast.makeText(InboxActivity.this,"No data found",Toast.LENGTH_SHORT).show();
+                }else {
+                    inboxPojos=response.body();
+                    inboxAdapter =new InboxAdapter(inboxPojos, InboxActivity.this);
+                    list_view.setAdapter(inboxAdapter);
+
+                }
+            }
+            @Override
+            public void onFailure(Call<List<InboxPojo>> call, Throwable t) {
+                progressDialog.dismiss();
+                Toast.makeText(InboxActivity.this, "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
