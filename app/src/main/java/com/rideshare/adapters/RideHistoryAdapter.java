737ab5f545a904922bdfffc076986cb609e8f0db
Hemanth-Reddy007 <41419298+Hemanth-Reddy007@users.noreply.github.com>
2021-02-26 14:31:47 -0500

    user dash board Inbox createdRide history created updated ride history

--- app/src/main/java/com/rideshare/adapters/RideHistoryAdapter.java
+++ app/src/main/java/com/rideshare/adapters/RideHistoryAdapter.java
@@ -14,15 +14,16 @@ import android.widget.TextView;
 
 import com.rideshare.R;
 import com.rideshare.activities.RideConfirmationActivity;
+import com.rideshare.models.MyRideHistoryPojo;
 import com.rideshare.models.RidesListPojo;
 
 import java.util.List;
 
 public class RideHistoryAdapter extends BaseAdapter {
-    List<RidesListPojo> ar;
+    List<MyRideHistoryPojo> ar;
     Context cnt;
 
-    public RideHistoryAdapter(List<RidesListPojo> ar, Context cnt) {
+    public RideHistoryAdapter(List<MyRideHistoryPojo> ar, Context cnt) {
         this.ar = ar;
         this.cnt = cnt;
     }
@@ -47,63 +48,23 @@ public class RideHistoryAdapter extends BaseAdapter {
         LayoutInflater obj1 = (LayoutInflater) cnt.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View obj2 = obj1.inflate(R.layout.list_rider_history_list, null);
 
-        TextView tv_vahicle = (TextView) obj2.findViewById(R.id.tv_vahicle);
-        tv_vahicle.setText("Vehicle : " + ar.get(pos).getSeats());
+        TextView tvFrom = (TextView) obj2.findViewById(R.id.tvFrom);
+        tvFrom.setText("From : " + ar.get(pos).getSource());
 
-        TextView tv_seats = (TextView) obj2.findViewById(R.id.tv_seats);
-        tv_seats.setText("Seats : " + ar.get(pos).getSeats());
+        TextView tvTo = (TextView) obj2.findViewById(R.id.tvTo);
+        tvTo.setText("To : " + ar.get(pos).getDestination());
 
-        TextView tv_price = (TextView) obj2.findViewById(R.id.tv_price);
-        tv_price.setText("Price : " + ar.get(pos).getSeats());
+        TextView tvAmount = (TextView) obj2.findViewById(R.id.tvAmount);
+        tvAmount.setText("Amount : " + ar.get(pos).getAmount()+"$");
 
-        TextView tv_aavailable = (TextView) obj2.findViewById(R.id.tv_aavailable);
-        tv_aavailable.setText("Available : " + ar.get(pos).getSeats());
+        TextView tvDate = (TextView) obj2.findViewById(R.id.tvDate);
+        tvDate.setText("Date : " + ar.get(pos).getDat());
 
 
-        Button btn_edit = (Button) obj2.findViewById(R.id.btn_edit);
-        btn_edit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(cnt, RideConfirmationActivity.class);
-                intent.putExtra("vehicle",ar.get(pos).getSeats());
-                intent.putExtra("seats",ar.get(pos).getSeats());
-                intent.putExtra("price",ar.get(pos).getSeats());
-                intent.putExtra("available",ar.get(pos).getSeats());
-                cnt.startActivity(intent);
+        TextView tvTime = (TextView) obj2.findViewById(R.id.tvTime);
+        tvTime.setText("Time : " + ar.get(pos).getTim());
 
-            }
-        });
 
-        Button btn_delete = (Button) obj2.findViewById(R.id.btn_delete);
-        btn_delete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AlertDialog.Builder builder1 = new AlertDialog.Builder(cnt);
-                builder1.setMessage("Do you want to Delte.");  //message we want to show the end user
-                builder1.setCancelable(true);
-
-                builder1.setPositiveButton(
-                        "Yes",
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                dialog.cancel(); //cancle the alert dialog box
-                                ((Activity)cnt).finish();
-                            }
-                        });
-
-                builder1.setNegativeButton(
-                        "No",
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                dialog.cancel();
-                            }
-                        });
-
-                AlertDialog alert11 = builder1.create();
-                alert11.show();
-
-            }
-        });
 
         return obj2;
     }
